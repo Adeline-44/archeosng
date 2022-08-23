ab67bc420ff265db65dc8295efa4dcc68b580810
Your Name <you@example.com>
2022-08-23 15:40:23 +0200

    premier commit

---
+++ src/Controller/HelpController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HelpController extends AbstractController
+{
+    #[Route('/help', name: 'help')]
+    public function index(): Response
+    {
+        return $this->render('help.html.twig', [
+        ]);
+    }
+}
